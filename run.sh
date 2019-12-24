2d6ec81aaf18faf5e89c3bead850246a5785b3d2
iBug <git@ibugone.com>
2019-12-24 19:54:17 +0800

    Set default User-Agent

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-exec scrapy runspider spider.py -o result.json -s USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
+if [ -z "$USER_AGENT" ]; then
+  USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
+fi
+
+exec scrapy runspider spider.py -o result.json -s USER_AGENT="$USER_AGENT"
