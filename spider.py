81fe7dad8f4046f1e914abd399bcd53d6a7dc557
iBug <git@ibugone.com>
2019-12-24 20:11:32 +0800

    Change UA, limit speed, fix typo

--- spider.py
+++ spider.py
@@ -6,17 +6,30 @@ import scrapy
 with open("users.txt", "r") as f:
     users = [int(line) for line in f]
 
-book_urls = [f"https://book.douban.com/people/{user}/collect&mode=list" for user in users]
-movie_urls = [f"https://movie.douban.com/people/{user}/collect&mode=list" for user in users]
+book_urls = [f"https://book.douban.com/people/{user}/collect?mode=list" for user in users]
+movie_urls = [f"https://movie.douban.com/people/{user}/collect?mode=list" for user in users]
 
 
 class DoubanSpider(scrapy.Spider):
     name = 'doubanspider'
     start_urls = book_urls + movie_urls
+    download_delay = 1  # 1 second between requests
 
     # These URLs are for testing only
     #start_urls = ["https://ustc.ibugone.com/data/book/1000030-0.html"]
     #start_urls = ["https://ustc.ibugone.com/data/movie/1000030-0.html"]
+
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
+        'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        'Accept-Encoding': "gzip, deflate",
+        'Accept-Language': "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+        'Referer': "https://www.douban.com/",
+    }
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse, headers=self.headers)
 
     def parse(self, response):
         userId = response.css('#db-usr-profile div.pic img').xpath('@src').get()
