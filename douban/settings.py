a5c0a9619a328c344ac25224f8937e16a8af5ab2
iBug <git@ibugone.com>
2019-12-25 15:56:25 +0800

    Don't follow 302

--- douban/settings.py
+++ douban/settings.py
@@ -25,6 +25,9 @@ ROBOTSTXT_OBEY = True
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
+# Don't follow 302
+REDIRECT_ENABLED=False
+
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
