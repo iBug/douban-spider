1a96fb25ef079bd4c99348f25ea9dd6db48c3fab
iBug <git@ibugone.com>
2019-12-25 13:30:25 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+set -e
+sed -Ei 's/stretch/buster/g' /etc/apt/sources.list
 export DEBIAN_FRONTEND=noninteractive
 apt-get update &&
-  apt-get upgrade &&
+  apt-get dist-upgrade -y &&
   apt-get install -y git python3-pip build-essential
 
+git clone git@github.com:iBug/douban-spider.git /root/douban-spider
 pip3 install scrapy fake-useragent requests
 
 mkdir -p $HOME/.config/systemd/user
